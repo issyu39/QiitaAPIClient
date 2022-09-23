17441c928cbc197df34bb6bf6886e9439c94196d
Yuichi <issyu039@gmail.com>
2022-09-23 12:14:54 +0900

    Qiita APIのアクセストークンを付与してリクエスト制限回数を増やした

--- app/src/main/java/com/example/issyu39/retrofitcoroutines/data/QiitaApiModule.kt
+++ app/src/main/java/com/example/issyu39/retrofitcoroutines/data/QiitaApiModule.kt
@@ -1,5 +1,6 @@
 package com.example.issyu39.retrofitcoroutines.data
 
+import com.example.issyu39.retrofitcoroutines.BuildConfig
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
@@ -7,7 +8,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -18,12 +22,30 @@ object QiitaApiModule {
     private val contentType = MediaType.get("application/json")
     private val json = Json { ignoreUnknownKeys = true }
 
+    class AddAuthorizationHeaderInterceptor : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer " + BuildConfig.QIITA_ACCESS_TOKEN)
+                .build()
+            return chain.proceed(request)
+        }
+    }
+
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+    fun provideOkhttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(AddAuthorizationHeaderInterceptor())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(json.asConverterFactory(contentType))
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .client(okHttpClient)
         .build()
 
     @Singleton
